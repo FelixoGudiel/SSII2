722d152d6e9fa4bc5689b0847cb2005976785eac
Felixo <felixangelgudiel@gmail.com>
2024-03-08 13:31:25 +0100

    comments

--- Client/clientsocket.py
+++ Client/clientsocket.py
@@ -12,7 +12,7 @@ HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 3030  # The port used by the server
 secret_key = b'my_secret_key'
 
-
+#Creación de la carpeta y el fichero NonceDB (si no existen), que almacena los nonces usados
 def crearNonceDB():
     directorio = "NonceDBClient"
     path = os.path.join(os.getcwd(), directorio)
@@ -21,8 +21,11 @@ def crearNonceDB():
         with open(os.path.join(path, "NonceDB.txt"), "w", encoding="utf-8") as file:
             file.write("")
             if os.name == "nt":
+                #Para mayor seguridad, este fichero se hace invisible
                 ctypes.windll.kernel32.SetFileAttributesW(path, FILE_ATTRIBUTE_HIDDEN)
                 
+#Se busca en la base de datos si el nonce ha sido usado previamente. Dado el espacio de búsqueda, es
+#improbable que se use el mismo dos veces, pero es conveniente comprobarlo.
 def comprobarNonce(nonce):
     directorio = "NonceDBClient"
     path = os.path.join(os.getcwd(), directorio)
@@ -36,6 +39,7 @@ def comprobarNonce(nonce):
         else:
             return False
 
+#Añade el nonce usado a la base de datos para que no pueda volver a ser usado.
 def escribirNonce(nonce):
     directorio = "NonceDBClient"
     path = os.path.join(os.getcwd(), directorio)
@@ -43,24 +47,39 @@ def escribirNonce(nonce):
         file.write(nonce)
         file.write("\n".encode())
 
-        
+#Proceso principal
+#Crear la base 
 crearNonceDB()
 
+#Conectar con el servidor
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    #El contenido del mensaje a mandar. Se debe cambiar esta variable en función de la información que se desea
+    #enviar.
     message = b"Cuenta1 Cuenta2 2000"
     
+    #Búsqueda de un nonce válido. Hasta que no se crea uno que no esté en la base de datos, no deja de crear.
     while (True):
         nonce = str(
+            #Los nonces posibles están entre 10^99 y 10^100-1, que son 9.e+99 posibilidades.
             random.randint(10**99, 10**100-1)
             ).encode("latin-1")
+        #Comprobación
         res = comprobarNonce(nonce)
+        #Si no está en la base de datos, se escoge ese nonce.
         if res is not False:
             break
     
+    #Se genera el resumen del mensaje y el nonce combinados.
     h = hmac.new(secret_key, message + nonce, hashlib.sha256)
-    mandar = message+ "delimitadordelimitadordelimitador".encode() + h.digest() + "delimitadordelimitadordelimitador".encode() + nonce
+    #Se mandan 3 valores separados por "delimitadordelimitadordelimitador". 
+    #Estos tres valores se combinan en una única cadena de bytes.
+    mandar = message+ "delimitadordelimitadordelimitador".encode()
+    + h.digest() + "delimitadordelimitadordelimitador".encode()
+    + nonce
+    #Se manda la cadena de bytes.
     s.sendall(mandar)
+    #Se recibe la respuesta del servidor
     data = s.recv(1024)
     print(data)
 
--- Server/serversocket.py
+++ Server/serversocket.py
@@ -11,6 +11,7 @@ HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
 secret_key = b'my_secret_key'
 
+#Creación de la carpeta y el fichero NonceDB (si no existen), que almacena los nonces usados
 def crearNonceDB():
     directorio = "NonceDBServer"
     path = os.path.join(os.getcwd(), directorio)
@@ -20,22 +21,23 @@ def crearNonceDB():
             file.write("")
             if os.name == "nt":
                 ctypes.windll.kernel32.SetFileAttributesW(path, FILE_ATTRIBUTE_HIDDEN)
-                
+
+#Se busca en la base de datos si el nonce ha sido usado previamente. Dado el espacio de búsqueda, es
+#improbable que se use el mismo dos veces, pero es conveniente comprobarlo.            
 def comprobarNonce(nonce):
     directorio = "NonceDBServer"
     path = os.path.join(os.getcwd(), directorio)
     with open(os.path.join(path, "NonceDB.txt"), "rb") as file:
         presente = False
         for line in file:
-            print(line)
-            print(nonce)
             if line[:-1]==nonce.encode():
                 presente = True
         if not presente:
             return False
         else:
             return True
-
+        
+#Añade el nonce usado a la base de datos para que no pueda volver a ser usado.
 def escribirNonce(nonce):
     directorio = "NonceDBServer"
     path = os.path.join(os.getcwd(), directorio)
@@ -43,27 +45,42 @@ def escribirNonce(nonce):
         file.write(nonce.encode())
         file.write("\n".encode())
            
-        
-crearNonceDB()             
+#Proceso principal
+#Crear la base 
+crearNonceDB()
+
+#Conectar con el servidor
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
+    #Acepta la conexión y espera a recibir datos.
     s.listen()
     conn, addr = s.accept()
     with conn:
+        #Address que se ha conectado.
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
+            #Decodifica el mensaje recibido.
             decoded = data.decode("latin-1")
+            #Separa los tres valores a recibir.
             partes = decoded.split("delimitadordelimitadordelimitador")
+            #Crea la función resumen a partir del mensaje y el nonce recibido.
             h = hmac.new(secret_key, partes[0].encode() + partes[2].encode(), hashlib.sha256)
+            respuesta = ""
+            #Comprueba si el resumen enviado coincide con el recreado.
             if (h.digest().decode('latin-1') == partes[1]):
+                #En caso positivo, se comprueba si el nonce ya ha sido usado.
                 presente = comprobarNonce(partes[2])
                 if not presente:
-                    conn.sendall(b'Bien')
+                    #En caso negativo, todo bien.
+                    respuesta = 'Bien'
                     escribirNonce(partes[2])
                 else:
-                    conn.sendall(b'replay!')
+                    #En caso positivo, es un replay.
+                    respuesta = 'replay!'
             else:
-                conn.sendall(b'Hash mal')
+                #En caso negativo, alguien ha alterado el contenido/hash.
+                respuesta = 'Hash mal'
+            
